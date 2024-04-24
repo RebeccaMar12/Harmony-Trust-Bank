35418319cf475c4f5d7491e34d8a7ae3fd8ffa3a
Joshua Salinas <joshsal792@gmail.com>
2024-04-24 17:57:00 -0400

    FINALKLYYYY

---
+++ main.py
@@ -0,0 +1,5 @@
+import mysql.connector
+
+connection = mysql.connector.connect(host = 'localhost', user = 'root', database = 'SmartCash', password = 'B!@ck_639928215')
+                                     
+connection.close()
